f7d0899fe0b0ea08cf2d78501bd41d470b5e90bb
Rishav Sharan <rishav.sharan@live.com>
2022-02-14 16:03:00 +0530

    Rvrtng 2 parseobj as options- none doesnt unpacks

--- src/compiler/parser.nim
+++ src/compiler/parser.nim
@@ -1,10 +1,14 @@
-import std/[strutils, strformat, times, options, sequtils, times]
+import std/[strutils, strformat, times, options, sequtils]
 
 var source* : string
 
 type 
     Parser =
         proc(): Option[string]
+
+    ParseObj = object
+        isSuccessful    : bool
+        parsed          : string
     
     ParseError* = object
         kind*           : string
@@ -133,6 +137,25 @@ proc parse* (code: string) : void =
                         return some(lpOut.get() & rpOut.get())
         )
 
+    # P1 - P2        # matches P1 if P2 does not match
+    proc `-` (lp: Parser, rp: Parser) : Parser =
+        return (
+            proc(): Option[string] =
+                let rpOut = rp()
+                if rpOut.isSome:
+                    echo "Right side was successful. This is wrong."
+                    return none(string)
+                else:
+                    let lpOut = lp()
+                    if lpOut.isSome():
+                        echo "left side was successful"
+                        return lpOut
+                    else:
+                        echo "both sides were unsuccessful"
+                        return none(string)
+
+        )
+
     # Zero or more repetitions of the given parser - always return success
     proc `*` (p: Parser) : Parser =
         return (
@@ -176,11 +199,47 @@ proc parse* (code: string) : void =
                     return none(string)
         )
 
-    remaining = "const  x = {something}"
-    echo "Starting string = ",  remaining
+    let lowerChar = c('a') | c('b') | c('c') | c('d') | c('e') | c('f') | c('g') | c('h') | c('i') | c('j') | c('k') | c('l') | c('m') | c('n') | c('o') | c('p') | c('q') | c('r') | c('s') | c('t') | c('u') | c('v') | c('w') | c('x') | c('y') | c('z')
+
+    let upperChar = c('A') | c('B') | c('C') | c('D') | c('E') | c('F') | c('G') | c('H') | c('I') | c('J') | c('K') | c('L') | c('M') | c('N') | c('O') | c('P') | c('Q') | c('R') | c('S') | c('T') | c('U') | c('V') | c('W') | c('X') | c('Y') | c('Z')
+
+    let alphabet = lowerChar | upperChar
+
+    let digit = c('0') | c('1') | c('2') | c('3') | c('4') | c('5') | c('6') | c('7') | c('8') | c('9')
+
+    let alphanum = alphabet | digit
+
+    let trueKwd = c('t') & c('r') & c('u') & c('e')
+    let falseKwd = c('f') & c('a') & c('l') & c('s') & c('e')
+
+    let boolDef =  trueKwd | falseKwd
 
     let varKwd = c('v') & c('a') & c('r')
     let constKwd = c('c') & c('o') & c('n') & c('s') & c('t')
+
+    let identifier = (alphabet & * ( alphanum | c('_'))) - (varKwd | constKwd | boolDef)
+
+    remaining = "somename"
+    pos = 0
+    let resIdent = identifier()
+    if resIdent.isSome():
+        echo "Success"
+        echo resIdent.get()
+    else:
+        echo "Failure"
+
+
+
+    # let declaration = const_decl | var_decl
+
+    # let statement = declareAndAssign | declaration | assignment | printStmt
+
+    # let program = * statement 
+
+
+    remaining = "const  x = {something}"
+    pos = 0
+    echo "Starting string = ",  remaining
     let declr = varKwd | constKwd | curse("Expected variable or constant declaration")
     let blockStmts = c('{') & anythingButC('}') & c('}')
     let chk = declr & + c(' ') & c('x') & c(' ') & c('=') & c(' ') & blockStmts
